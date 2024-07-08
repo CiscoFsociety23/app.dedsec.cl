d47998939c9160060ce3ebef3d5fd9ff9da1cb94
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-07-08 02:13:06 -0400

    feature: ingress plataform

--- app_dedsec_cl/ContentView.swift
+++ app_dedsec_cl/ContentView.swift
@@ -8,28 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    func getAccess(){
+        print("Botón presionado")
+    }
+    
     var body: some View {
-        ZStack {
-            Color(red: 0.176, green: 0.196, blue: 0.314)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                Text("Dedsec Corp")
-                    .font(.title)
-                    .foregroundColor(Color(red: 112/255, green: 119/255, blue: 161/255))
-                Text("Mobile App")
-                    .font(.subheadline)
-                    .foregroundColor(Color(red: 112/255, green: 119/255, blue: 161/255))
-                Button(action: {
-                    print("Botón presionado")
-                }) {
-                    Text("Ingresar")
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 10)
-                        .background(Color(red: 112/255, green: 119/255, blue: 161/255))
-                        .foregroundColor(Color(red: 45/255, green: 50/255, blue: 80/255))
-                        .cornerRadius(20)
+        NavigationView {
+            ZStack {
+                Color(red: 45/255, green: 50/255, blue: 80/255)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Dedsec Corp")
+                        .font(.title)
+                        .foregroundColor(Color(red: 112/255, green: 119/255, blue: 161/255))
+                    Text("Mobile App")
+                        .font(.subheadline)
+                        .foregroundColor(Color(red: 112/255, green: 119/255, blue: 161/255))
+                    NavigationLink(destination: DedsecIngress()){
+                        Text("Ingresar")
+                            .padding()
+                            .foregroundColor(Color(red: 45/255, green: 50/255, blue: 80/255))
+                            .background(Color(red: 112/255, green: 119/255, blue: 161/255))
+                            .cornerRadius(20)
+                    }
                 }
-                .padding(.top, 10)
             }
         }
     }
